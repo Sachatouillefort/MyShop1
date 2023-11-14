164bace1be1dd733903edba2724ed47e90db0144
Sachatouillefort <sacha.gaulin@epitech.digital>
2023-11-14 11:54:11 +0100

    all

--- my-shop-api/vue-project/src/views/Admin.vue
+++ my-shop-api/vue-project/src/views/Admin.vue
@@ -1,12 +1,13 @@
 <template>
-    <section>
-      <h1>ADMINdrtyfugihugyftdyr</h1>
-    </section>
-  </template>
-  
-  <script>
-  </script>
-   
-  <style>
-  </style>
-  
+    <div>
+        <h1>ADMIN</h1>
+    </div>
+</template>
+
+<script>
+
+</script>
+
+<style>
+    
+</style>
